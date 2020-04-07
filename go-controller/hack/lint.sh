1f487c0d000bfc9770c329bd0189e6803024eeeb
Jacob Tanenbaum <jtanenba@redhat.com>
2020-04-07 09:24:36 -0400

    Infrastructure to autogenerate files
    script that generates the CRD, deepcopy, informers, listers, and clientset and set the linter exclude the auto-generated files
    
    Signed-off-by: Jacob Tanenbaum <jtanenba@redhat.com>

--- go-controller/hack/lint.sh
+++ go-controller/hack/lint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 GO111MODULE=on ${GOPATH}/bin/golangci-lint run \
+    --skip-dirs=pkg/crd/egressfirewall/v1/apis/ \
     --tests=false --enable gofmt \
     --timeout=10m0s \
     && echo "lint OK!"
